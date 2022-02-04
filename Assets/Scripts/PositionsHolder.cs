aa1ed44ca9fc6b89b5a2a9282d87fa1e4a786a0a
Gleb Demenov <glebadem@gmail.com>
2022-02-04 21:04:35 +0300

    Позиция эффектов

--- Assets/Scripts/PositionsHolder.cs
+++ Assets/Scripts/PositionsHolder.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PositionsHolder : MonoBehaviour
 {
     public Transform swordPos;
+    public Transform effectPos;
     public Transform beltPos;
     public Transform crystalPos;
 }
